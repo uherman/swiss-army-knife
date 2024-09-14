06270665f436d5c9e7187a3539de4b8735f909dc
Herman Brunberg <herman.brunberg@mio.se>
2024-09-14 20:39:04 +0200

    fix(about): correct bg class

--- src/routes/tools/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import CardSection from '@/components/custom/card-section/card-section.svelte';
-	import MainContent from '@/components/custom/layout/main-content.svelte';
-	import { tools } from '@/utils/constants';
-</script>
-
-<MainContent>
-	<CardSection cards={tools} />
-</MainContent>
