846a02d7c23c5cbb83af75ec2a80e8744eb3e477
Herman Brunberg <herman.brunberg@mio.se>
2024-09-14 17:23:25 +0200

    feat(about): add content and styling

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,38 @@
 <script lang="ts">
 	import MainContent from '@/components/custom/layout/main-content.svelte';
+	import Button from '@/components/ui/button/button.svelte';
 </script>
 
 <MainContent>
-	<h1 class="mb-3 text-3xl">About</h1>
+	<div class=" flex items-center justify-center">
+		<div class="mx-4 rounded-lg bg-inherit bg-white p-8">
+			<h1 class="mb-6 text-4xl font-bold">About</h1>
+			<p class="mb-6 max-w-2xl text-lg">
+				This website is a collection of tools and utilities that I've built to help me with my
+				day-to-day tasks. Feel free to explore and use them as you see fit.
+			</p>
 
-	<p class="text-lg text-gray-700 lg:w-1/2 dark:text-gray-300">
-		Welcome to the swiss army knife. This website is a collection of tools and utilities that I've
-		built to help me with my day-to-day tasks. Feel free to explore and use them as you see fit. If
-		you have any feedback or suggestions, please don't hesitate to reach out. Enjoy!
-	</p>
+			<h2 class="mb-4 text-2xl font-semibold">Contribute & Collaborate</h2>
+			<p class="mb-6 max-w-2xl text-lg">
+				Have an idea for a new tool? Found a bug? I encourage you to contribute to the project!
+				Open-source collaboration is at the heart of this platform. You can submit issues, suggest
+				improvements, and contribute code on <a
+					href="https://github.com/uherman/swiss-army-knife"
+					class="text-blue-500 hover:underline"
+					target="_blank">GitHub</a
+				>.
+			</p>
+
+			<div class="mt-8">
+				<Button
+					variant="link"
+					class="text-muted-foreground px-0 hover:text-current"
+					href="https://github.com/uherman/swiss-army-knife/issues/new/choose"
+					target="_blank"
+				>
+					Create issue on GitHub
+				</Button>
+			</div>
+		</div>
+	</div>
 </MainContent>
