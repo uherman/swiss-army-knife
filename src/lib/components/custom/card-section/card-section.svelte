1bd7bac6fe6f1c38f118ea2e20bf9ba64a3702cb
Herman Brunberg <herman.brunberg@mio.se>
2024-09-14 21:07:57 +0200

    feat(theme): change tw theme

--- src/lib/components/custom/card-section/card-section.svelte
+++ src/lib/components/custom/card-section/card-section.svelte
@@ -13,12 +13,12 @@
 
 <section
 	class={cn(
-		'bg-muted mt-10 grid gap-4 rounded-lg p-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4',
+		'md:bg-muted/50 mt-10 grid w-full justify-center gap-4 rounded-lg p-4 md:w-auto md:grid-cols-2 md:border lg:grid-cols-3 xl:grid-cols-4 dark:md:border-none',
 		className
 	)}
 >
 	{#each cards as card}
-		<Card.Root class="w-[300px]">
+		<Card.Root class="bg-muted w-[300px]">
 			<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
 				<Card.Title class="text-sm font-medium">{card.title}</Card.Title>
 				<svelte:component this={card.icon} class=" text-muted-foregroundh-4 w-4" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,23 @@
 	import CardSection from '@/components/custom/card-section/card-section.svelte';
 	import GithubLink from '@/components/custom/github-link/github-link.svelte';
 	import { tools } from '@/utils/constants';
+	import { Separator } from '$lib/components/ui/separator';
 </script>
 
 <MainContent>
-	<GithubLink />
+	<div class="flex w-full items-center justify-start">
+		<div class="mx-2 rounded-lg p-4 md:mx-4 md:p-8">
+			<h1 class="mb-6 text-4xl font-bold">Swiss Army Knife</h1>
+			<p>
+				This website is a collection of tools and utilities that I've built to help me with my
+				day-to-day tasks. Feel free to explore and use them as you see fit.
+			</p>
+		</div>
+	</div>
 	<CardSection cards={tools} />
+
+	<Separator class="mx-auto my-10 w-4/5" />
+	<footer class="mb-2">
+		<GithubLink />
+	</footer>
 </MainContent>
